f6cb867ec3144a96fa6d4824e614cbbb6144331b
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-06-07 05:39:01 +0530

    redux file setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-xl bg-slate-700 h-screen flex items-center justify-center'>
+      <h1 className='text-white'>Todo List by using Redux setup</h1>
+    </div>
+  )
+}
+
+export default App
